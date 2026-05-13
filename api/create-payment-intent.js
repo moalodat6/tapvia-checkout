d37c49d86b01f3840ab06ea0e76b72557ae77c7f
moalodat6 <moalodat6@gmail.com>
2026-05-13 07:56:29 -0400

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 6999,
+    amount: 1,
     currency: 'usd',
     automatic_payment_methods: { enabled: true },
   });
